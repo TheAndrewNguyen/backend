612031b6438ebf640a0ea146eede50be3d95f07c
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-04-25 16:17:46 -0700

    cors implemented

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = 3000
 
-// iomprot route files 
+// cors
+app.use(cors()); 
+
+// import route files 
 const apiRouter = require('./routes/api.js')
 
 app.use("/api", apiRouter); 
