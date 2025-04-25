3523404301609dea5cc7cfee80a894b9374db8ff
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2025-04-25 16:54:21 -0700

    to json

---
+++ controllers/apiController.js
@@ -0,0 +1,21 @@
+
+
+const getBasicInfo = async(req, res) => {
+    try {
+        const address = req.params.address; 
+
+    res.status(200).json(
+        {
+            "address": address, 
+            "date": "5/6/2025", 
+            "name": "Seattle General Election", 
+            "regDead": "5/5/2025", 
+            "regLink": "https://seattle.gov/"
+        }
+    )
+    } catch {
+        res.status(500).json({"message" : "failure has occured"})
+    }
+}
+
+module.exports = {getBasicInfo}
--- routes/api.js
+++ routes/api.js
@@ -3,31 +3,16 @@
 const express = require('express')
 const router = express.Router()
 
+const getBasicInfoController = require("../controllers/apiController")
+
+
 // test api
 router.get('/', (req, res)=> {
     res.send("Api Parsing data from Google Civic API"); 
 })
 
-// get the location 
-router.get('/location', (req, res)=>{
-    res.send("Zipcode / Location Information")
-})
 
-// get election name
-router.get('/electionName', (req, res)=>{
-    res.send("Election Name: ")
-})
-
-// get regestration Deadline 
-router.get('/regDeadline', (req, res)=>{
-    res.send("regDeadline")
-})
-
-// get regestration link 
-router.get(('/regLink'), (req, res)=>{
-    res.send("regLink"); 
-})
-
+router.get('/getBasicInfo/:address', getBasicInfoController.getBasicInfo); 
 
 
 module.exports = router
